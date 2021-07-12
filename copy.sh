5946007422f044f8804145532cec4de12b65817f
Àͱþ¿¡ <laobingjun@takshine.com>
2021-07-12 23:16:15 +0800

    update

--- copy.sh
+++ copy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rclone copy onedrive: gdrive_mirror:Frank --transfers 48 -v --ignore-errors
+rclone copy onedrive_business: onedrive:Onedrive_business --transfers=48 -v --ignore-errors --exclude "{.svn,.git,.idea}/" --drive-acknowledge-abuse --buffer-size=64M -u --no-modtime
