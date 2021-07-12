27e5943bc2d4afb23f2ec9a3448527711bf1fa17
MeditatorV <47035563+MeditatorV@users.noreply.github.com>
2021-07-12 23:34:52 +0800

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rclone copy onedrive_besiness: onedrive:Onedrive_business --transfers=48 -v --ignore-errors --exclude "{.svn,.git,.idea}/" --drive-acknowledge-abuse --buffer-size=64M -u
+rclone copy onedrive_besiness: onedrive:Onedrive_business --transfers=24 -v --ignore-errors --exclude "{.svn,.git,.idea}/" --drive-acknowledge-abuse --buffer-size=64M -u
