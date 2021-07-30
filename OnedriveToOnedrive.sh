9339f0622eec6b25b83f8f1fc310369ffef4eaf5
MeditatorV <47035563+MeditatorV@users.noreply.github.com>
2021-07-30 22:17:11 +0800

    Update OnedriveToOnedrive.sh

--- OnedriveToOnedrive.sh
+++ OnedriveToOnedrive.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rclone  --checksum sync onedrive_besiness: onedrive:Onedrive_business --check-first --checkers=48 --no-traverse --transfers=48 --buffer-size=128M --onedrive-chunk-size=25600 --ignore-errors --ignore-checksum --ignore-size  --ignore-existing --exclude "{.svn,.git,.idea}/" --drive-acknowledge-abuse  -v -u
+rclone  --checksum sync onedrive_besiness: onedrive:Onedrive_business --checkers=48 --no-traverse --transfers=48 --buffer-size=128M --onedrive-chunk-size=25600 --ignore-errors --ignore-checksum --ignore-size  --ignore-existing --exclude "{.svn,.git,.idea}/" --drive-acknowledge-abuse  -v -u
