7333446a1d987bd570203976b42e2699d0627d90
MeditatorV <47035563+MeditatorV@users.noreply.github.com>
2021-08-19 18:36:25 +0800

    Update OnedriveToGoogle.sh

--- OnedriveToGoogle.sh
+++ OnedriveToGoogle.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rclone sync onedrive_besiness: gdrive:Onedrive_business --checkers=48 --transfers=48 --transfers=48 --buffer-size=128M --onedrive-chunk-size=25600 --multi-thread-cutoff=258M --multi-thread-streams=10 --ignore-errors --ignore-checksum --ignore-size --exclude "{.svn,.git,.idea}/" --drive-acknowledge-abuse  -v -u
+rclone copy onedrive_besiness: gdrive:Onedrive_business --checkers=48 --transfers=48 --transfers=48 --buffer-size=128M --onedrive-chunk-size=25600 --multi-thread-cutoff=258M --multi-thread-streams=10 --ignore-errors --ignore-checksum --ignore-size --exclude "{.svn,.git,.idea}/" --drive-acknowledge-abuse  -v -u
