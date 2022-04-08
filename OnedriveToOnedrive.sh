a931537a30563bb878bcc17083859efea8913842
MeditatorV <47035563+MeditatorV@users.noreply.github.com>
2022-04-08 22:55:43 +0800

    Update OnedriveToOnedrive.sh

--- OnedriveToOnedrive.sh
+++ OnedriveToOnedrive.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rclone --check-first --order-by modtime,ascending --refresh-times sync onedrive_business: onedrive:Onedrive_business --checkers=48 --transfers=48 --use-mmap --buffer-size=128M --onedrive-chunk-size=128M --multi-thread-cutoff=258M --multi-thread-streams=10 --ignore-errors --exclude "{.svn,.git,.idea}/" --drive-acknowledge-abuse  -v -u
+rclone --check-first --order-by modtime,ascending --refresh-times sync onedrive_business: onedrive:Onedrive_business --checkers=48 --transfers=48 --use-mmap --buffer-size=128M --onedrive-chunk-size=250M --multi-thread-cutoff=258M --multi-thread-streams=10 --ignore-errors --exclude "{.svn,.git,.idea}/" --drive-acknowledge-abuse  -v -u
