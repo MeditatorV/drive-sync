c019b99ac4e552e38ecbc5c7a0404df26b87f2fb
MeditatorV <47035563+MeditatorV@users.noreply.github.com>
2022-04-08 12:12:10 +0800

    Update and rename OnedriveToOnedrive.sh to OnedriveToOnedrive_bit.sh

---
+++ OnedriveToOnedrive_bit.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+rclone sync --size-only onedrive_business: onedrive_bit: --checkers=48 --transfers=48 --buffer-size=128M --onedrive-chunk-size=25600 --multi-thread-cutoff=258M --multi-thread-streams=10 --ignore-errors --exclude "{.svn,.git,.idea}/" --drive-acknowledge-abuse  -v -u
