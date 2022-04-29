dc06707e9341911de7682a8bbf7b4612596c57d1
MeditatorV <47035563+MeditatorV@users.noreply.github.com>
2022-04-29 18:43:37 +0800

    Update OnedriveToOnedrive_junmark.sh

--- OnedriveToOnedrive_junmark.sh
+++ OnedriveToOnedrive_junmark.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rclone sync --size-only onedrive_business: onedrive_junmark: --checkers=64 --transfers=4  --buffer-size=1024M --onedrive-chunk-size=102400 --multi-thread-cutoff=1024M --multi-thread-streams=10 --ignore-errors --exclude "{.svn,.git,.idea}/" --drive-acknowledge-abuse  -v -u
+rclone  --fast-list --order-by modtime,desc sync --size-only  onedrive_business: onedrive_junmark: --checkers=64 --transfers=4  --buffer-size=1024M --onedrive-chunk-size=102400 --multi-thread-cutoff=1024M --multi-thread-streams=10 --ignore-errors --exclude "{.svn,.git,.idea}/" --drive-acknowledge-abuse  -v -u
