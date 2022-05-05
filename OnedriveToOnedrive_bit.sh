5e85840008e7f50ddfccd7f2f6a495aa6564a0e0
MeditatorV <47035563+MeditatorV@users.noreply.github.com>
2022-05-05 13:50:25 +0800

    Update OnedriveToOnedrive_bit.sh

--- OnedriveToOnedrive_bit.sh
+++ OnedriveToOnedrive_bit.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rclone  --size-only --fast-list sync onedrive_business: onedrive_bit: -u --checkers=24 --transfers=2 --use-mmap --buffer-size=128M --cache-chunk-size=5M --onedrive-chunk-size=100M --multi-thread-cutoff=250M --multi-thread-streams=4 --ignore-errors --exclude "{.svn,.git,.idea}/" --drive-acknowledge-abuse  -v 
+rclone --fast-list --order-by modtime,desc sync --size-only onedrive_business: onedrive_bit: --checkers=64 --transfers=4 --buffer-size=1024M --onedrive-chunk-size=102400 --multi-thread-cutoff=1024M --multi-thread-streams=10 --exclude "{.svn,.git,.idea}/" --drive-acknowledge-abuse  -v -u
